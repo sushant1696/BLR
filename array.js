bc96b38ecaf6a4a95def8545cdb237ce4b38edaf
sushant1696 <shaan160196@gmail.com>
2019-12-18 17:39:15 +0530

    []first

---
+++ array.js
@@ -0,0 +1,10 @@
+/*var superheros=['anand','praven','katappa','kiran'];
+console.log(superheros);
+console.log(typeof(superheros));
+*/
+
+var superheros=['anand','praven','katappa','kiran'];
+for(item in superheros){
+console.log(superheros);
+}
+console.log(typeof(superheros));
