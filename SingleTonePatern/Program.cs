29e7cab02fcfe72ba7b8b4846dc60c33059f2a56
sushant1696 <shaan160196@gmail.com>
2020-01-24 19:07:30 +0530

    [sushant]Adding the singletone program

---
+++ SingleTonePatern/Program.cs
@@ -0,0 +1,16 @@
+﻿using DesignPatern.SingleTonePatern;
+using System;
+
+namespace DesignPatern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleTone obj = SingleTone.MyObject();
+            obj.MyMethod();
+            SingleTone obj1 = SingleTone.MyObject();
+            obj1.MyMethod();
+        }
+    }
+}
---
+++ SingleTonePatern/SingleTone.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=Singletone.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="sushanta das"/>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatern.SingleTonePatern
+{
+    /// <summary>
+    /// Creat a class named Singletone of dwsign patern
+    /// </summary>
+    sealed class SingleTone
+    {
+        public static int count = 0;
+        public static SingleTone Instance;
+        private SingleTone()
+        {
+            count++;
+            Console.WriteLine(count);
+        }
+
+        /// <summary>
+        /// Mies the object.
+        /// </summary>
+        /// <returns></returns>
+        public static SingleTone MyObject()
+        {
+            if (Instance == null)
+            {
+                return new SingleTone();
+            }
+            return Instance;
+        }
+        /// <summary>
+        /// Mies the method.
+        /// </summary>
+        public void MyMethod()
+        {
+            Console.WriteLine("this my method");
+        }
+    }
+}
