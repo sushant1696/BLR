20eb711b6d79e481bfb33454cf8ac6e4e9f77741
sushant1696 <shaan160196@gmail.com>
2020-01-28 12:33:42 +0530

    [sushant]Add.adding and implementing basic program

---
+++ ObjectAsReturnTypeAndArgument/EmpService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basic.ObjectAsReturnTypeAndArgument
+{
+    class EmpService
+    {
+        /// <summary>
+        /// Saves the specified e.
+        /// employe as a argument
+        /// </summary>
+        /// <param name="e">The e.</param>
+        public void Save(Employee e)
+        {
+            Console.WriteLine(e.getName()+""+e.getId());
+        }
+        /// <summary>
+        /// Gets the emp.
+        /// employee as a return type
+        /// </summary>
+        /// <returns></returns>
+        public Employee getEmp()
+        {
+            Employee obj = new Employee(101, "shan");
+            return obj;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,76 +1,18 @@
-﻿using DesignPattern.Facade;
-using DesignPattern.FactoryDesignPattern;
-using DesignPattern.PrototypeDesign;
+﻿using Basic.ObjectAsReturnTypeAndArgument;
 using System;
 
-namespace DesignPattern
+namespace Basic
 {
     class Program
     {
-        private static Facade.Facade facobj;
-
-        public static void DisplayValues(Person p)
+       public static void Main(string[] args)
         {
-            Console.WriteLine("      Name: {0:s}, Age: {1:d}, BirthDate: {2:MM/dd/yy}",
-                p.Name, p.age, p.Date);
-            Console.WriteLine(" ID#: {0:d}", p.Idinfo.IdNumber);
-        }
-        [Obsolete]
-        static void Main(string[] args)
-        {
-        
-            Console.WriteLine("1->factory design");
-            Console.WriteLine("2->prototype design");
-            Console.WriteLine("3-> facade design pattern");
-            Console.WriteLine("choose your option");
+            Employee ob = new Employee(234,"ddd");
             
-            int k = Convert.ToInt32(Console.ReadLine());
-
-            switch (k) 
-            {
-                case 1:
-            TestFactory.IsFactory();
-                    break;
-                case 2:
-                    Person p1 = new Person();
-                    p1.age = 23;
-                    p1.Date = Convert.ToDateTime("1996-01-16");
-
-                    p1.Name = "shan";
-                    p1.Idinfo = new IdInfo(101);
-
-                    // Perform a shallow copy of p1 and assign it to p2
-                    Person P2 = p1.ShallowCopy();
-                    // Make a deep copy of p1 and assign it to p3.
-                    Person P3 = p1.DeepCopy();
-
-                    Console.WriteLine("Original values of p1, p2, p3:");
-                    Console.WriteLine("   p1 instance values: ");
-                    DisplayValues(p1);
-                    Console.WriteLine("   p2 instance values:");
-                    DisplayValues(P2);
-                    Console.WriteLine("   p3 instance values:");
-                    DisplayValues(P3);
-                    // Change the value of p1 properties and display the values of p1,
-                    // p2 and p3.
-                    p1.age = 32;
-                    p1.Date = Convert.ToDateTime("1900-01-01");
-                    p1.Name = "Frank";
-                    p1.Idinfo.IdNumber = 7878;
-                    Console.WriteLine("\nValues of p1, p2 and p3 after changes to p1:");
-                    Console.WriteLine("   p1 instance values: ");
-                    DisplayValues(p1);
-                    Console.WriteLine("   p2 instance values (reference values have changed):");
-                    DisplayValues(P2);
-                    Console.WriteLine("   p3 instance values (everything was kept the same):");
-                    DisplayValues(P3);
-                  
-
-                    break;
-                case 3:
-                    Client.ClientMethod(facobj);
-                    break;
-        }
+            EmpService obj = new EmpService();
+                obj.Save(ob);
+            Employee e = obj.getEmp();
+            Console.WriteLine(e.getId() + "" + e.getName());
         }
     }
 }
