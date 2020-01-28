7da9cb904c9276df632ac520c8674939c22f7846
sushant1696 <shaan160196@gmail.com>
2020-01-28 17:23:57 +0530

    [sushant]Adding the design pattern program

---
+++ AdapterDesign/AdapterTeast.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=AdapterDesignPtern.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="sushanta das"/>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.AdapterDesign
+{
+    /// <summary>
+    /// create a Adapter test class
+    /// </summary>
+    class AdapterTeast
+    {
+        /// <summary>
+        /// Adapters the test method.
+        /// </summary>
+        public static void AdapterTestMethod()
+        {
+            AdapterInharit obj = new AdapterInharit();
+            Voltsc volt220obj = obj.Get220Volts();
+            Voltsc volt320obj = obj.Get320Volts();
+            Voltsc volt50obj = obj.Get50Volts();
+            ////print all the values
+            Console.WriteLine(" volts : " + volt50obj.GetVolts());
+            Console.WriteLine(" volts : " + volt220obj.GetVolts());
+            Console.WriteLine(" volts : " + volt320obj.GetVolts());
+        }
+    }
+}
---
+++ FactoryDesignPattern/TestFactory.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=FactoryDesignPattern.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="sushanta das"/>
+// ----------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.FactoryDesignPattern
+{
+    /// <summary>
+    /// Create a class named TestFactory
+    /// </summary>
+    class TestFactory
+    {
+        public static void IsFactory()
+        {
+            Console.WriteLine("enter computer type");
+            string type = Console.ReadLine();
+            Computer obj = ComputerFactory.IsComputerFactory(type);
+            if (obj != null)
+            {
+               
+                obj.Charging();
+                
+                obj.NotCharging();
+            
+                obj.Running();
+            }
+            else
+            {
+                
+                Console.WriteLine("enter a valid computer type");
+            }
+        }
+    }
+}
