4d7440c54174c673ecc54265e8f75fb5b9eb3a54
sushant1696 <shaan160196@gmail.com>
2020-01-28 16:55:07 +0530

    [sushant]Add.adding and implementing the mediator design patern

---
+++ MediatorDesign/ConcreteUser.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=MediatorDesignPtern.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="sushanta das"/>
+// ----------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.MediatorDesign
+{
+    /// <summary>
+    /// create a class whic inherit the User class
+    /// this class impliment the two method of User class
+    /// </summary>
+    /// <seealso cref="DesignPattern.MediatorDesign.User" />
+    public class ConcreteUser:User
+    {
+        public ConcreteUser(FacebookGroupMediator mediator, string name) : base(mediator, name)
+        {
+        }
+            public override void Receive(string message)
+        {
+            Console.WriteLine(this.name + ": Received Message:" + message);
+        }
+        public override void Send(string message)
+        {
+            Console.WriteLine(this.name + ": Sending Message=" + message + "\n");
+            mediator.SendMessage(message, this);
+        }
+
+    }
+}
---
+++ MediatorDesign/User.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=MediatorDesignPtern.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="sushanta das"/>
+// ----------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.MediatorDesign
+{
+    /// <summary>
+    /// create a abstract class which have two abstract method
+    /// send and Receive
+    /// </summary>
+    public abstract class User
+    {
+        protected FacebookGroupMediator mediator;
+        protected string name;
+        public User(FacebookGroupMediator mediator, string name)
+        {
+            this.mediator = mediator;
+            this.name = name;
+        }
+        public abstract void Send(string message);
+        public abstract void Receive(string message);
+    }
+}
