de133b7d7fdfefc3327d968e29e1813dc91e7bb7
sushant1696 <shaan160196@gmail.com>
2020-02-21 19:07:21 +0530

    [sushant]Refactore .added the usecase2 problem

---
+++ MoodAnalysisNunitTest/UseCase2/UseCase2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NunitTestInMoodAnalysis.UseCase2
+{
+    class UseCase2
+    {
+        string message;
+
+        public UseCase2()
+        {
+        }
+
+        public UseCase2(string msg)
+        {
+            this.message = msg;
+        }
+        public static string MoodAnalyse_ReturnMethod3(string message)
+        {
+            try
+            {
+                if (message.ToLower().Contains("sad"))
+                {
+                    return "SAD";
+                }
+                else (message.ToLower().Contains("happy"))
+                {
+                    return "HAPPY";
+                }
+                
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "happy";
+            }
+            return null;
+        }
+
+        public string MoodAnalyse_ReturnMethod()
+        {
+            if (message.ToLower().Contains("sad"))
+            {
+                return "SAD";
+            }
+            else if (message.ToLower().Contains("happy"))
+            {
+                return "HAPPY";
+            }
+            else
+                return "HAPPY";
+        }
+    }
+}
