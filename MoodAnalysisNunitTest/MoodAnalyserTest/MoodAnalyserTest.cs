e2ab2e6be7cc3508a24dc58fcfcde9213313c182
sushant1696 <shaan160196@gmail.com>
2020-02-21 18:36:31 +0530

    [sushant] Add .added the MoodAnalysisProblem

---
+++ MoodAnalysisNunitTest/MoodAnalyserTest/MoodAnalyserTest.cs
@@ -0,0 +1,51 @@
+
+using NUnit.Framework;
+using NunitTestInMoodAnalysis.usecase1;
+using System;
+
+namespace MoodAnalysisNunitTest
+{
+    public class Tests
+    {
+        [Test]
+        public void GivenSadMessage_WhenAnalyse_ReturnSad()
+        {
+            string obj = UseCase1.MoodAnalyse_ReturnMethod("i am sad");
+            Assert.AreEqual("SAD", obj);
+        }
+        [Test]
+        public void GivenHappyMessage_WhenAnalyse_Returnhappy()
+        {
+            string obj = UseCase1.MoodAnalyse_ReturnMethod("i am happy");
+            Assert.AreEqual("HAPPY", obj);
+        }
+        [Test]
+        public void GivenAnyMoodMessage_WhenAnalyse_Returnhappy()
+        {
+            string obj = UseCase1.MoodAnalyse_ReturnMethod("i am happy");
+            Assert.AreEqual("HAPPY", obj);
+        }
+        [Test]
+        public void GivenSadMessage_WhenAnalyse_ReturnSad2()
+        {
+            UseCase1 obj = new UseCase1("i am sad");
+            string result =obj.MoodAnalyse_ReturnMethod();
+            Assert.AreEqual("SAD", result);
+        }
+        [Test]
+        public void GivenHappyMessage_WhenAnalyse_Returnhappy2()
+        {
+            UseCase1 obj = new UseCase1("i am Happy");
+            string result = obj.MoodAnalyse_ReturnMethod();
+            Assert.AreEqual("HAPPY", result);
+        }
+        [Test]
+        public void GivenAnyMoodMessage_WhenAnalyse_Returnhappy2()
+        {
+            UseCase1 obj = new UseCase1("i am Happy");
+            string result = obj.MoodAnalyse_ReturnMethod();
+            Assert.AreEqual("HAPPY", result);
+        }
+
+    }
+}
---
+++ MoodAnalysisNunitTest/usecase1/UseCase1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NunitTestInMoodAnalysis.usecase1
+{
+    class UseCase1
+    {
+        string message;
+
+        public UseCase1()
+        {
+        }
+
+        public UseCase1(string msg)
+        {
+            this.message = msg;
+        }
+        public static string MoodAnalyse_ReturnMethod(string message)
+        {
+            if (message.ToLower().Contains("sad"))
+            {
+                return "SAD";
+            }
+            else if (message.ToLower().Contains("happy"))
+            {
+                return "HAPPY";
+            }
+            else
+                return "HAPPY";
+        }
+
+        public string MoodAnalyse_ReturnMethod()
+        {
+            if (message.ToLower().Contains("sad"))
+            {
+                return "SAD";
+            }
+            else if (message.ToLower().Contains("happy"))
+            {
+                return "HAPPY";
+            }
+            else
+                return "HAPPY";
+        }
+    }
+}
